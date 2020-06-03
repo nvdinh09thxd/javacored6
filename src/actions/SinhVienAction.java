4b8dec60d390032b8d39b4a716a88f8501d3a9a9
nvdinh09thxd <nvdinh09thxd@gmail.com>
2020-06-03 21:58:10 +0700

    add phu test

--- src/actions/SinhVienAction.java
+++ src/actions/SinhVienAction.java
@@ -17,10 +17,35 @@ public class SinhVienAction {
 		return sv;
 	}
 
-	public static void display(SinhVien2 sv) {
+	public static SinhVien2[] input(int n) {
+		SinhVien2[] arSV = new SinhVien2[n];
+		Scanner sc = new Scanner(System.in);
+		for (int i = 0; i < arSV.length; i++) {
+			System.out.print("Nhap ho ten: ");
+			String name = sc.nextLine();
+			System.out.print("Nhap dia chi: ");
+			String address = sc.nextLine();
+			System.out.print("Nhap truong: ");
+			String school = sc.nextLine();
+			SinhVien2 objSV = new SinhVien2(name, address, school);
+			arSV[i] = objSV;
+		}
+		return arSV;
+	}
+
+	public static void displayData(SinhVien2 sv) {
 		// System.out.println("Ho ten: " + sv.getName());
 		// System.out.println("Dia chi: " + sv.getAddress());
 		// System.out.println("Truong: " + sv.getSchool());
 		System.out.println(sv.toString());
+	}
+
+	public static void display(SinhVien2[] arSV) {
+		System.out.println("Danh sach cac sinh vien");
+		for (SinhVien2 sv : arSV) {
+			System.out.println("Ho ten: " + sv.getName());
+			System.out.println("Dia chi: " + sv.getAddress());
+			System.out.println("Truong: " + sv.getSchool());
+		}
 	}
 }
--- src/test/SinhVienTest.java
+++ src/test/SinhVienTest.java
@@ -1,19 +1,28 @@
 package test;
 
 import bean.SinhVien2;
+
+import java.util.Scanner;
+
 import actions.SinhVienAction;
 
 public class SinhVienTest {
 
 	public static void main(String[] args) {
-		SinhVien2[] arSV = new SinhVien2[3];
-		for (int i = 0; i < arSV.length; i++) {
-			System.out.println("Nhap thong tin sinh vien thu " + (i + 1));
-			arSV[i] = SinhVienAction.inputData();
-		}
-		for (int i = 0; i < arSV.length; i++) {
-			System.out.println("Xuat thong tin sinh vien thu " + (i + 1));
-			SinhVienAction.display(arSV[i]);
-		}
+		// SinhVien2[] arSV = new SinhVien2[3];
+		// for (int i = 0; i < arSV.length; i++) {
+		// System.out.println("Nhap thong tin sinh vien thu " + (i + 1));
+		// arSV[i] = SinhVienAction.inputData();
+		// }
+		// for (int i = 0; i < arSV.length; i++) {
+		// System.out.println("Xuat thong tin sinh vien thu " + (i + 1));
+		// SinhVienAction.displayData(arSV[i]);
+		// }
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhap so sinh vien: ");
+		int n = sc.nextInt();
+		SinhVien2[] arSV2 = SinhVienAction.input(n);
+		SinhVienAction.display(arSV2);
 	}
 }
